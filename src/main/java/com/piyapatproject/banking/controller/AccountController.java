16d0a7b6bb6ceea897c9bedc6927f2d48be399f5
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-06 21:44:21 +0700

    Build Deposit Account REST API

--- src/main/java/com/piyapatproject/banking/controller/AccountController.java
+++ src/main/java/com/piyapatproject/banking/controller/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
@@ -22,9 +24,20 @@ public class AccountController {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     }
 
+    // Get Account REST API
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
         AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    // Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id,
+                                              @RequestBody Map<String, Double> request) {
+
+        Double amount = request.get("amount");
+        AccountDto accountDto = accountService.deposit(id, amount);
         return ResponseEntity.ok(accountDto);
     }
 }
