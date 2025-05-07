b7f6bb5a481c1254f4f9a3a73724ee5a4a62a191
Piyapat <piyapat.thongrueng@gmail.com>
2025-05-07 11:31:33 +0700

    Refactor using Java Records As DTO

--- src/main/java/com/piyapatproject/banking/dto/AccountDto.java
+++ src/main/java/com/piyapatproject/banking/dto/AccountDto.java
@@ -1,13 +1,19 @@
 package com.piyapatproject.banking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AccountDto {
+//
+//    private Long id;
+//    private String accountHolderName;
+//    private double balance;
+//}
 
-@Data
-@AllArgsConstructor
-public class AccountDto {
+public record AccountDto(Long id,
+                         String accountHolderName,
+                         double balance) {
 
-    private Long id;
-    private String accountHolderName;
-    private double balance;
 }
